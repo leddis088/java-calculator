2962d2e0fd4427a123f1c15360cfd25c2611b635
Feddis08 <felixriemer@Felixs-MacBook-Pro.fritz.box>
2023-04-24 22:03:13 +0200

    added a line

--- src/start.java
+++ src/start.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class start {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Please enter a number: ");
         int number = sc.nextInt();
         System.out.println("Please enter the operation: ");
