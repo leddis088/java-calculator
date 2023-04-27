50e1eb035f65ab1a1122ce8081807d3ae9cfaff0
leddis088 <leon.m.2001@gmx.de>
2023-04-27 15:56:16 +0200

    added a calculate_function && added a loop

--- src/start2.java
+++ src/start2.java
@@ -2,26 +2,36 @@ import java.util.Scanner;
 
     public class start2 {
         public static void main(String[] args) {
+            System.out.println("Hello, in the Leddis08 CALCULATOR!!!!!");
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your calculation: ");
-            String command = sc.next();
-            String[] opts = command.split(";");
+            while (true){
+                System.out.println("Enter your calculation or type 'x' to exit.");
+                String command = sc.next();
+                if (command.equals("x") || command.equals("X")){
+                    System.out.println("Leddis08 says 'Bye! Come and calculate again!'");
+                    break;
+                }
+                String[] opts = command.split(";");
+                int result = calculate(Integer.parseInt(opts[0]), Integer.parseInt(opts[2]), opts[1]);
+                System.out.println(" = " + result);
+            }
+
+        }
+
+        public static int calculate(int number_1, int number_2, String opt){
             int result = 0;
-
-            if (opts[1].equals("+")) {
-                result = (Integer.parseInt(opts[0]) + Integer.parseInt(opts[2]));
+            if (opt.equals("+")) {
+                result = (number_1 + number_2);
             }
-            if (opts[1].equals("-")) {
-                result = (Integer.parseInt(opts[0]) - Integer.parseInt(opts[2]));
+            if (opt.equals("-")) {
+                result = (number_1 - number_2);
             }
-            if (opts[1].equals("*")) {
-                result = (Integer.parseInt(opts[0]) * Integer.parseInt(opts[2]));
+            if (opt.equals("*")) {
+                result = (number_1 * number_2);
             }
-            if (opts[1].equals("/")) {
-                result = (Integer.parseInt(opts[0]) / Integer.parseInt(opts[2]));
+            if (opt.equals("/")) {
+                result = (number_1 / number_2);
             }
-
-            System.out.println(" = " + result);
-
+            return result;
         }
     }
